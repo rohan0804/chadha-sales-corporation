57de1002e2275dbe968558a6a246bb885ae643a9
sahil kumar <sahil.kumar@codequotient.com>
2020-07-29 12:23:20 +0530

    nab

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("chadhasalescorporation", "root", "1234", {
+const sequelize = new Sequelize("chadhasalescorporation", "root", "", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
